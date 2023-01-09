30c63b36740534f826ba3a8362a28f153424e9c5
Gaurav Joshi <gj451026@gmail.com>
2023-01-09 10:44:19 +0530

    favicon added and loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import instagram from "../assets/instagram.webp"
-
+import meta from "../assets/meta.webp"
 import "../styling/LoadingScreen.scss"
 
 const LoadingScreen = () => {
@@ -8,6 +8,13 @@ const LoadingScreen = () => {
     <div className='LoadingScreen_container'>
         <div className="LoadingScreen_meta-logo_container">
             <img src={instagram} alt="meta" />
+        </div>
+        <div className="LoadingScreen_meta-from_container">
+          <div className="LoadingScreen_meta-name_container">
+            <p><img src={meta} alt="meta" /> Meta</p>
+            <p>&nbsp;|&nbsp;</p>
+            <p>Gaurav Joshi</p>
+          </div>
         </div>
     </div>
   )
