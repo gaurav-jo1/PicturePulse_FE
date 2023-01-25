035109566818a70f88e7c7b4227c08284d7ba5a3
Gaurav Joshi <gj451026@gmail.com>
2023-01-25 23:11:54 +0530

    UI updated

--- src/components/Getapp.jsx
+++ src/components/Getapp.jsx
@@ -12,12 +12,8 @@ const Getapp = () => {
       <p>Get the app</p>
       </div>
       <div className="Login_container_get_instagram_store-images">
-        <a href="https://apps.apple.com/in/app/instagram/id389801252">
           <img src={apple_store} alt="apple_store" />
-        </a>
-        <a href="https://play.google.com/store/apps/details?id=com.instagram.android&hl=en_IN&gl=US">
           <img src={play_store} alt="Play_store" />
-        </a>
       </div>
     </div>
   );
